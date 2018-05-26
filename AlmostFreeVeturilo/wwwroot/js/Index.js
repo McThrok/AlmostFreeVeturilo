75bd1c3bdc53b719d927836dcabe24c92a9f206b
Anastasia <khlebousa@gmail.com>
2018-05-26 21:52:13 +0200

    correct marker labels
    Start Point - A, Destination - B, other have bike's count

--- AlmostFreeVeturilo/wwwroot/js/Index.js
+++ AlmostFreeVeturilo/wwwroot/js/Index.js
@@ -1,4 +1,5 @@
-﻿var map;
+﻿
+var map;
 var currentStartStations = [];
 var startMarkers = [];
 var i = 0;
@@ -9,7 +10,7 @@ var endLocMarker;
 var currentLocationMarker;
 
 var minBikesAtStation = 3;
-var timeFactor = 2
+var timeFactor = 2;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'),
@@ -46,9 +47,9 @@ function initMap() {
     }
     //
     var reset = document.getElementById("reset");
-    reset.onclick = function() {
+    reset.onclick = function () {
         activeteStartListening();
-        pathMarkers.forEach(function(m) { m.setMap(null); });
+        pathMarkers.forEach(function (m) { m.setMap(null); });
         pathMarkers.length = 0;
 
         endLocMarker.setMap(null);
@@ -62,23 +63,58 @@ function initMap() {
         destination.classList.add(LOCKED);
     };
 
+    //TODO uncomment
+    $.ajax({
+        url: "http://localhost:50588/api/Path/",
+        dataType: "json",
+        success: function (data) {
+            //console.log(data);
+            for (var i = 0; i < data.length; i++) {
+                var station = data[i];
+                //console.log(station.bikes)
+                markerForBasicStation(station.lat, station.lng, station.name, station.bikes)
+            }
+        }
+    });
+
     var settingsIcon = document.getElementById("settingsIcon");
     var settingsDiv = document.getElementById("settings");
+    settingsDiv.style.display = "none";
     settingsIcon.onclick = function () {
         if (settingsDiv.style.display === "none")
             settingsDiv.style.display = "block";
         else
             settingsDiv.style.display = "none";
     }
+    // MARKERS
+    function correctBikesCount(bikes) {
+        if (bikes == 0) bikes = "O";
+        if (bikes > 10) bikes = 10;
 
-    function createMarker(lat, lng, name, iconColor) {
+        return bikes;
+    }
+
+    function markerForBasicStation(lat, lng, name, bikes) {
+        bikes = correctBikesCount(bikes);
+
         return marker = new google.maps.Marker({
             position: { lat: lat, lng: lng },
             map: map,
             title: name,
-            icon: "http://maps.google.com/mapfiles/ms/icons/" + iconColor + "-dot.png"
+            icon: "http://maps.google.com/mapfiles/kml/paddle/" + bikes + "-lv.png"
         });
     }
+    function createMarker(lat, lng, name, bikes) {
+        bikes = correctBikesCount(bikes);
+
+        return marker = new google.maps.Marker({
+            position: { lat: lat, lng: lng },
+            map: map,
+            title: name,
+            icon: "http://maps.google.com/mapfiles/kml/paddle/" + bikes + ".png"
+        });
+    }
+    //
 
     function activeteStartListening() {
         google.maps.event.addListener(map, 'click', function (e) {
@@ -95,19 +131,19 @@ function initMap() {
             url: "http://localhost:50588/api/Path/" + lat + "/" + lng + "/" + minBikesAtStation,
             dataType: "json",
             success: function (data) {
-                currentLocationMarker = createMarker(lat, lng, "Start point", "green");
+                currentLocationMarker = createMarker(lat, lng, "Start point", "A");
                 var startStationsMarkers = [];
 
                 data.forEach(function (pathPart) {
                     var station = pathPart.station;
-                    var marker = createMarker(station.lat, station.lng, "qwe", "yellow");
+                    var marker = createMarker(station.lat, station.lng, station.name, station.bikes);
                     startStationsMarkers.push(marker);
 
                     marker.addListener('click', function () {
                         startStationsMarkers.forEach(function (m) { m.setMap(null); });
                         startStationsMarkers.length = 0;
 
-                        showChosenStation(currentLocationMarker, station.uid, station.lat, station.lng);
+                        showChosenStation(currentLocationMarker, station);
                     });
                 });
 
@@ -115,25 +151,24 @@ function initMap() {
         });
     }
 
-    function showChosenStation(currLocMarker, uid, lat, lng) {
+    function showChosenStation(currLocMarker, station) {
         firstStation.classList.add(DONE);
         destination.classList.remove(LOCKED);
 
-        var chosenStationMarker = createMarker(lat, lng, "qwe", "blue");
+        var chosenStationMarker = createMarker(station.lat, station.lng, station.name, station.bikes);
         google.maps.event.addListener(map, 'click', function (e) {
             google.maps.event.clearListeners(map, 'click');
             destination.classList.add(DONE);
-
-            endLocMarker = createMarker(e.latLng.lat(), e.latLng.lng(), "qwe", "red");
+            endLocMarker = createMarker(e.latLng.lat(), e.latLng.lng(), "Destination", "B");
 
             $.ajax({
-                url: "http://localhost:50588/api/Path/" + uid + "/" + e.latLng.lat() + "/" + e.latLng.lng() + "/" + minBikesAtStation + "/" + timeFactor,
+                url: "http://localhost:50588/api/Path/" + station.uid + "/" + e.latLng.lat() + "/" + e.latLng.lng() + "/" + minBikesAtStation + "/" + timeFactor,
                 dataType: "json",
                 success: function (data) {
                     pathMarkers.push(chosenStationMarker);
                     data.stations.forEach(function (station, index) {
                         if (index !== 0)
-                            pathMarkers.push(createMarker(station.lat, station.lng, "qwe", "blue"));
+                            pathMarkers.push(createMarker(station.lat, station.lng, station.name, station.bikes));
                     });
 
                     //activeteStartListening();
